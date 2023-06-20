efdd08f8a76cfb113337dbca27efb9b2189d1974
Micael Santana Windows <micaelparadox@gmail.com>
2023-06-20 10:47:16 -0300

    routes

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
+use Illuminate\Http\Middleware\EnsureTokenIsValid;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AutorController;
+use App\Http\Controllers\LivroController;
 
 /*
 |--------------------------------------------------------------------------
@@ -9,11 +11,23 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/autores', [AutorController::class, 'apiIndex']);
+Route::post('/autores', [AutorController::class, 'apiStore']);
+Route::get('/autores/{id}', [AutorController::class, 'apiShow']);
+Route::put('/autores/{id}', [AutorController::class, 'apiUpdate']);
+Route::delete('/autores/{id}', [AutorController::class, 'apiDestroy']);
+
+Route::get('/livros', [LivroController::class, 'apiIndex']);
+Route::post('/livros', [LivroController::class, 'apiStore']);
+Route::get('/livros/{id}', [LivroController::class, 'apiShow']);
+Route::put('/livros/{id}', [LivroController::class, 'apiUpdate']);
+Route::delete('/livros/{id}', [LivroController::class, 'apiDestroy']);
